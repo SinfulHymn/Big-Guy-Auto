e14ff3854737406c7521ae8114959708eb6ecd10
Alejandro Rojas <kmachappy@pm.me>
2022-10-25 02:18:40 -0700

    test

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 
-const Layout = ({ children }) => {
+type LayoutProps = {
+    children: React.ReactNode;
+};
+
+const Layout = ({children}: LayoutProps) =>  {
     return (
         <div className="flex flex-col h-screen">
             <Header />
