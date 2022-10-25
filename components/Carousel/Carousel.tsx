a7cbcf762dbb38b4570e69dff287a6c79b56150b
Alejandro Rojas <kmachappy@pm.me>
2022-10-25 03:17:21 -0700

    fixed deployment

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -14,60 +14,61 @@ import "swiper/css/navigation";
 const Carousel = () => {
     return (
 
-            <Swiper
-                className="relative w-full max-h-[350px] sm:max-h-[500px] h-full"
-                // effect={"fade"}
-                spaceBetween={0}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 8500,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    type: "progressbar",
+        <Swiper
+            className="relative w-full max-h-[350px] sm:max-h-[400px] md:max-h-[500px]  lg:max-h-[600px] h-full transition-all duration-500 ease-in-out"
+            // effect={"fade"}
+            spaceBetween={0}
+            centeredSlides={true}
+            autoplay={{
+                delay: 8000,
+                disableOnInteraction: false,
+            }}
+            pagination={{
+                type: "progressbar",
 
-                    clickable: true,
-                }}
-                navigation={true}
-                // fadeEffect={{ crossFade: true }}
-                modules={[Autoplay, Pagination, Navigation, EffectFade]}
-                slidesPerView={1}
-                // onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
+                clickable: true,
+            }}
+            navigation={true}
+            // fadeEffect={{ crossFade: true }}
+            modules={[Autoplay, Pagination, Navigation, EffectFade]}
+            slidesPerView={1}
+        // onSlideChange={() => console.log('slide change')}
+        // onSwiper={(swiper) => console.log(swiper)}
+        >
 
-                {Array.from({ length }, (_, index) => (
+            {Array.from({ length }, (_, index) => (
 
-                    <SwiperSlide key={index} className="relative flex min-w-full h-full">
-                        <div className="absolute flex flex-col justify-center items-center left-1/2 right-1/2 top-1/2 bottom-1/2">
-                            <div className="bg-opacity-70 text-white p-4 bg-carousel-title1 my-5 whitespace-nowrap fluid-base sm:fluid-3xl">
-                                <span className="mr-2 font-medium">
-                                    San Fernando
-                                </span>
-                                <span className="font-light">
-                                    Auto Collision Repair
-                                </span>
-
-                            </div>
-                            <div className=" bg-opacity-80 font-light text-main-red p-4 bg-carousel-title2 my-5 whitespace-nowrap fluid-base sm:fluid-3xl">
-                                San Fernando Auto Repair
-                            </div>
-
-
+                <SwiperSlide key={index} className="relative flex min-w-full h-full transition-all">
+                    <div className="absolute flex flex-col justify-center items-center left-1/2 right-1/2 top-1/2 bottom-1/2">
+                        <div className=" text-white p-3 sm:p-4 bg-carousel-title1 my-6 whitespace-nowrap fluid-base sm:fluid-3xl">
+                            <span className="mr-2 font-medium">
+                                San Fernando
+                            </span>
+                            <span className="font-light">
+                                Auto Collision Repair
+                            </span>
 
                         </div>
-                        <Image
-                            
-                            className=" object-cover w-full h-auto"
-                            src={imagesByIndex(index)}
-                            alt="Big Guy Auto Body"
-                            sizes="100vw"
+                        <div className=" bg-opacity-80 font-light text-main-red p-3 my-6 sm:p-4 bg-carousel-title2  whitespace-nowrap fluid-base sm:fluid-3xl">
+                            San Fernando Auto Repair
+                        </div>
 
-                        />
-                    </SwiperSlide>
-                ))}
 
-            </Swiper>
+
+                    </div>
+                    <Image
+
+                        className=" object-cover w-full h-auto"
+                        src={imagesByIndex(index)}
+                        alt="Big Guy Auto Body"
+                        sizes="100vw"
+                        quality={100}
+
+                    />
+                </SwiperSlide>
+            ))}
+
+        </Swiper>
 
     );
 };
