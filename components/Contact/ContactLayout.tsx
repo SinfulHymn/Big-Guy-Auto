6d2092480d5a3d967f90cde1b10794d8a75b08d8
Alejandro Rojas <kmachappy@pm.me>
2022-12-03 11:33:39 -0800

    more styling changes to contact form

--- components/Contact/ContactLayout.tsx
+++ components/Contact/ContactLayout.tsx
@@ -8,17 +8,23 @@ const Contact = () => {
       <Banner title="Contact Us" />
       <div className="py-5 sm:py-10">
         <div className="container mx-auto grid grid-cols-12 gap-5">
-          <div className="col-span-9">
+          <div className="col-span-9 space-y-6">
+
             <div className="text-xl font-light">
               Leave a Message
               <hr className="my-4 h-px bg-gray-200 border-0 dark:bg-gray-700"></hr>
             </div>
+            
+            <div>
             <p className=' text-lg font-normal'>
               Get in touch with us!
             </p>
+            </div>
+
             <div>
               <EmailComponent />
             </div>
+          
           </div>
           <div className="col-span-3">
             <div className="text-xl font-light">Company Info</div>
--- components/Contact/EmailComponent.tsx
+++ components/Contact/EmailComponent.tsx
@@ -1,72 +1,108 @@
-import { useState } from "react"
-export default function EmailComponent(){
-    const [form, setForm] = useState({
-        name: "",
-        email: "",
-        subject: "",
-        phone: "",
-        message: ""
-    })
-    const handleForm = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        })
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(form)
-    }
-    
-    return(
-        <form action="">
-                <div className="grid grid-cols-12 gap-5">
-                  <div className="col-span-6">
-                    <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-                      Name
-                    </label>
-                    <div className="mt-2">
-                      <input
-                      placeholder="Name"
-                        value={form.name}
-                        onChange={handleForm}
-                        type="text"
-                        name="name"
-                        id="name"
-                        className="shadow-sm focus:ring-main-red focus:border-main-red block w-full sm:text-sm border-gray-300 rounded-md h-8"
-                      />
-                    </div>
-                  </div>
-                  <div className="col-span-6">
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                      Email
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        className="shadow-sm focus:ring-main-red focus:border-main-red block w-full sm:text-sm border-gray-300 rounded-md h-8"
-                      />
-                    </div>
-                  </div>
-                  <div className="col-span-12">
-                    <label htmlFor="message" className="block text-sm font-medium text-gray-700">
-                      Message
-                    </label>
-                    <div className="mt-1">
-                      <textarea
+import { useState } from 'react';
+export default function EmailComponent() {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    phone: '',
+    message: '',
+  });
+  const handleForm = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
 
-                        id="message"
-                        name="message"
-                        rows={4}
-                        className="shadow-sm focus:ring-main-red focus:border-main-red block w-full sm:text-sm border-gray-300 rounded-md"
-                        defaultValue={''}
+  return (
+    <form action="">
+      <div className="grid grid-cols-12 gap-5 ">
+        <div className="col-span-6 flex flex-col space-y-8">
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-semibold text-gray-700"
+            >
+              Name
+            </label>
+            <div className="mt-2">
+              <input
+                placeholder="Enter your name"
+                value={form.name}
+                onChange={handleForm}
+                type="text"
+                name="name"
+                id="name"
+                className="shadow-sm focus:ring-main-red focus:border-main-red block w-full sm:text-sm border-gray-300 rounded-sm h-[50px]"
+              />
+            </div>
+          </div>
 
-                      />
-                    </div>
-                  </div>
-                  </div>
-              </form>
-    )
+          <div >
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-700"
+            >
+              Email
+            </label>
+            <div className="mt-2">
+              <input
+                placeholder="Enter your email"
+                value={form.email}
+                onChange={handleForm}
+                type="email"
+                name="email"
+                id="email"
+                className="shadow-sm focus:ring-main-red focus:border-main-red block w-full sm:text-sm border-gray-300 rounded-sm h-[50px]"
+              />
+            </div>
+          </div>
+          <div >
+            <label
+              htmlFor="subject"
+              className="block text-sm font-semibold text-gray-700"
+            >
+              Subject
+            </label>
+            <div className="mt-2">
+              <input
+                placeholder="Enter a subject"
+                value={form.subject}
+                onChange={handleForm}
+                type="text"
+                name="subject"
+                id="subject"
+                className="shadow-sm focus:ring-main-red focus:border-main-red block w-full sm:text-sm border-gray-300 rounded-sm h-[50px]"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-6 ">
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Message
+          </label>
+          <div className="mt-1">
+            <textarea
+              id="message"
+              name="message"
+              rows={10}
+              
+              value={form.message}
+              onChange={handleForm}
+              className="shadow-sm focus:ring-main-red focus:border-main-red block w-full sm:text-sm border-gray-300 rounded-sm h-[272px]"
+              defaultValue={''}
+            />
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 }
